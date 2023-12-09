54423926fa99e88c8336be7ac9407eddef734ddf
Lewis King <lewis.king@imgarena.com>
2023-12-09 10:03:57 +0000

    part1 day9

---
+++ day9/mirage_maintenance.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"advent-of-code-2023/common"
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	lines := common.ReadFileLines("day9/input/input.txt")
+	total := 0
+	for _, line := range lines {
+		words := strings.Fields(line)
+		var numbers []int
+		for _, word := range words {
+			num, _ := strconv.Atoi(word)
+			numbers = append(numbers, num)
+		}
+		nextValue := calculateNextValue(numbers, numbers[len(numbers)-1])
+		fmt.Println(nextValue)
+		total += nextValue
+	}
+	fmt.Println(total)
+}
+
+func calculateNextValue(numbers []int, lastNum int) int {
+	var diffs []int
+	for i := 0; i < len(numbers)-1; i++ {
+		diffs = append(diffs, numbers[i+1]-numbers[i])
+	}
+	if checkAllSame(diffs) {
+		return lastNum + diffs[len(diffs)-1]
+	} else {
+		return numbers[len(numbers)-1] + calculateNextValue(diffs, diffs[len(diffs)-1])
+	}
+}
+
+func checkAllSame(numbers []int) bool {
+	for i := 0; i < len(numbers)-1; i++ {
+		if numbers[i] != numbers[i+1] {
+			return false
+		}
+	}
+	return true
+}
---
+++ init_day.sh
@@ -0,0 +1,6 @@
+echo "Initialising folder structure for day $1"
+
+mkdir -p $1
+mkdir -p $1/input
+touch $1/input/input.txt
+touch $1/input/example.txt
