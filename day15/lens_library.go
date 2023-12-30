58a290ec9a1c21b4879750536cab5ff7a3b61d08
Lewis King <lewis.king@imgarena.com>
2023-12-30 18:59:57 +0000

    day15 partb

--- day15/lens_library.go
+++ day15/lens_library.go
@@ -3,17 +3,78 @@ package main
 import (
 	"advent-of-code-2023/common"
 	"fmt"
+	"strconv"
 	"strings"
 )
 
 func main() {
 	lines := common.ReadFileLines("day15/input/input.txt")
 	initSequences := strings.Split(lines[0], ",")
+
+	// Part 1
 	total := 0
 	for _, initSequence := range initSequences {
 		total += runHashAlgorithm(initSequence)
 	}
 	fmt.Println(total)
+
+	// Part 2
+	boxesAndLenses := populateBoxes(initSequences)
+	focusingPower := calculateFocusingPower(boxesAndLenses)
+	fmt.Println(focusingPower)
+}
+
+func calculateFocusingPower(boxesAndLenses map[int][]LabelAndLens) int {
+	totalFocusingPower := 0
+	for box, lenses := range boxesAndLenses {
+		for slot, focalLength := range lenses {
+			focusingPower := (box + 1) * (slot + 1) * focalLength.lens
+			fmt.Println(focusingPower)
+			totalFocusingPower += focusingPower
+		}
+	}
+	return totalFocusingPower
+}
+
+func populateBoxes(initSequences []string) map[int][]LabelAndLens {
+	boxesAndLenses := make(map[int][]LabelAndLens)
+	for _, initSequence := range initSequences {
+		if strings.Contains(initSequence, "=") {
+			label := strings.Split(initSequence, "=")[0]
+			box := runHashAlgorithm(label)
+			lenses := boxesAndLenses[box]
+			exists := false
+			for index, lens := range lenses {
+				if lens.label == label {
+					focal, _ := strconv.Atoi(strings.Split(initSequence, "=")[1])
+					newLens := LabelAndLens{label, focal}
+					lenses[index] = newLens
+					exists = true
+				}
+			}
+			if !exists {
+				focal, _ := strconv.Atoi(strings.Split(initSequence, "=")[1])
+				newLens := LabelAndLens{label, focal}
+				boxesAndLenses[box] = append(lenses, newLens)
+			}
+		} else if strings.Contains(initSequence, "-") {
+			label := strings.Split(initSequence, "-")[0]
+			box := runHashAlgorithm(label)
+			lenses := boxesAndLenses[box]
+			for index, lens := range lenses {
+				if lens.label == label {
+					boxesAndLenses[box] = append(lenses[:index], lenses[index+1:]...)
+				}
+			}
+		}
+		fmt.Println(boxesAndLenses)
+	}
+	return boxesAndLenses
+}
+
+type LabelAndLens struct {
+	label string
+	lens  int
 }
 
 func runHashAlgorithm(sequence string) int {
